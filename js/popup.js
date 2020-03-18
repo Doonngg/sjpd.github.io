d22ec5f99682455dfa150c97e35057b4d85b154e
Doonngg <61999334+Doonngg@users.noreply.github.com>
2020-03-18 17:24:58 +0800

    12

--- js/popup.js
+++ js/popup.js
@@ -29,63 +29,63 @@ $(function() {
 			"font-size": "12px"
 		})
 	})
-	
+
+
+	//创建时间
 	var time = new Date();
-	document.onkeydown = function send(e){
-		if(e.keyCode == 13){
-			if ($("#text").val() == "输入内容") {
-				$("#text").val("");
-			}
-			//判断输入长度
-			if ($("#text").val().length == 0 || /^\s+$/.test($("#text").val())) {
-				$("#tips").css("display", "block");
-				setTimeout(function() {
-					$("#tips").css("display", "none");
-				}, 1000);
-				return;
-			}
-			//弹幕设置面板隐藏
-			$(".dmset-show").css("display", "none");
-			//弹幕数量统计显示
-			$("#dn-num").fadeIn();
-			//创建弹幕&设置弹幕样式
-			var dmLi = $("<li></li>").attr("class", "dmLi");
-			var dmLeft = $("<div></div>").attr("class", "dmLeft");
-			dmLi.append(dmLeft);
-			var dmRight = $("<div></div>").attr("class", "dmRight");
-			dmLi.append(dmRight);
-			//头像
-			var dmImg = $("<img>").attr("class", "dmImg").attr("src", $("#tx img").attr("src"));
-			dmLeft.append(dmImg);
-			let dmRight1 = $("<div></div>").attr("class", "dmRight1");
-			dmRight.append(dmRight1);
-			var dmId = $("<p></p>").attr("class", "dmId").text($("#id").text());
-			dmRight1.append(dmId);
-			let report = $("<img>").attr("src", "img/report.png");
-			dmRight1.append(report);
-			var dmTime = $("<p></p>").attr("class", "dmTime").text(time.toLocaleString());
-			dmRight.append(dmTime);
-			if (/^https?:\/\/\w+\.\w+.\w+/.test($("#text").val())) {
-				var dmUrl = $("<a></a>").attr("class", "dmUrl").attr("href", $("#text").val()).text("进入链接");
-				dmRight.append(dmUrl);
-			} else {
-				var dmText = $("<p></p>").attr("class", "dmText").text($("#text").val());
-				dmRight.append(dmText);
-			}
-			//把弹幕放进ul
-			$("#tab1").append(dmLi);
-			//滚轮保持底部
-			$("ul").scrollTop($("ul")[0].scrollHeight);
-			//弹幕数量统计
-			$("#dmNum").text($("#tab1 li").length);
+	//弹幕发射
+	$("#send").click(function() {
+		if ($("#text").val() == "输入内容") {
 			$("#text").val("");
-			
-			$(".dmRight1 img").click(function() {
-				console.log("举报");
-			});
-		};
-	}
-	
+		}
+		//判断输入长度
+		if ($("#text").val().length == 0 || /^\s+$/.test($("#text").val())) {
+			$("#tips").css("display", "block");
+			setTimeout(function() {
+				$("#tips").css("display", "none");
+			}, 1000);
+			return;
+		}
+		//弹幕设置面板隐藏
+		$(".dmset-show").css("display", "none");
+		//弹幕数量统计显示
+		$("#dn-num").fadeIn();
+		//创建弹幕&设置弹幕样式
+		var dmLi = $("<li></li>").attr("class", "dmLi");
+		var dmLeft = $("<div></div>").attr("class", "dmLeft");
+		dmLi.append(dmLeft);
+		var dmRight = $("<div></div>").attr("class", "dmRight");
+		dmLi.append(dmRight);
+		//头像
+		var dmImg = $("<img>").attr("class", "dmImg").attr("src", $("#tx img").attr("src"));
+		dmLeft.append(dmImg);
+		let dmRight1 = $("<div></div>").attr("class", "dmRight1");
+		dmRight.append(dmRight1);
+		var dmId = $("<p></p>").attr("class", "dmId").text($("#id").text());
+		dmRight1.append(dmId);
+		let report = $("<img>").attr("src", "img/report.png");
+		dmRight1.append(report);
+		var dmTime = $("<p></p>").attr("class", "dmTime").text(time.toLocaleString());
+		dmRight.append(dmTime);
+		if (/^https?:\/\/\w+\.\w+.\w+/.test($("#text").val())) {
+			var dmUrl = $("<a></a>").attr("class", "dmUrl").attr("href", $("#text").val()).text("进入链接");
+			dmRight.append(dmUrl);
+		} else {
+			var dmText = $("<p></p>").attr("class", "dmText").text($("#text").val());
+			dmRight.append(dmText);
+		}
+		//把弹幕放进ul
+		$("#tab1").append(dmLi);
+		//滚轮保持底部
+		$("ul").scrollTop($("ul")[0].scrollHeight);
+		//弹幕数量统计
+		$("#dmNum").text($("#tab1 li").length);
+		$("#text").val("");
+
+		$(".dmRight1 img").click(function() {
+			console.log("举报");
+		})
+	});
 
 
 	//tab切换
