ccc0d7b6602271d7c4a080b629ae3d684c94f66e
Doonngg <61999334+Doonngg@users.noreply.github.com>
2020-03-19 14:41:35 +0800

    1

---
+++ js/lrrj.js
@@ -0,0 +1,45 @@
+$(function(){
+	let happyflag = 0;
+	$("#happy").click(function(){
+		if(happyflag == 0){
+			$("#happy img").attr("src","img/bq/happy1.png");
+			happyflag = 1;
+		}else {
+			$("#happy img").attr("src","img/bq/happy.png");
+			happyflag = 0;
+		}
+	})
+	let calmflag = 0;
+	$("#calm").click(function(){
+		if(calmflag == 0){
+			$("#calm img").attr("src","img/bq/calm1.png");
+			calmflag = 1;
+		}else{
+			$("#calm img").attr("src","img/bq/calm.png");
+			calmflag = 0;
+		}
+		
+	})
+	let sadflag = 0;
+	$("#sad").click(function(){
+		if(sadflag == 0){
+			$("#sad img").attr("src","img/bq/sad1.png");
+			sadflag = 1;
+		} else{
+			$("#sad img").attr("src","img/bq/sad.png");
+			sadflag = 0;
+		}
+		
+	})
+	let angerflag = 0;
+	$("#anger").click(function(){
+		if(angerflag == 0){
+			$("#anger img").attr("src","img/bq/anger1.png");
+			angerflag = 1;
+		} else{
+			$("#anger img").attr("src","img/bq/anger.png");
+			angerflag = 0;
+		}
+		
+	})
+})
